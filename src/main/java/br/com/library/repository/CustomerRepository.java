ffbd7e19d206ab0d9df4e8b7222339f5ef325f00
Jusley Souza <jusleysouza31@gmail.com>
2023-01-06 17:44:47 -0300

    Adding the repository annotation

--- src/main/java/br/com/library/repository/CustomerRepository.java
+++ src/main/java/br/com/library/repository/CustomerRepository.java
@@ -3,9 +3,11 @@ package br.com.library.repository;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.library.model.Customer;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, UUID> {
 
 }
