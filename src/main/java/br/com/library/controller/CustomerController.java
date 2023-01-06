612fb51519153e637960ebae95de09a1c5e50245
Jusley Souza <jusleysouza31@gmail.com>
2023-01-06 18:32:11 -0300

    Creation and implementation of the create route

--- src/main/java/br/com/library/controller/CustomerController.java
+++ src/main/java/br/com/library/controller/CustomerController.java
@@ -6,10 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.library.model.Customer;
+import br.com.library.model.dto.RequestDTO;
 import br.com.library.services.CustomerService;
 
 @RestController
@@ -22,6 +25,11 @@ public class CustomerController {
 	@GetMapping
 	public ResponseEntity<List<Customer>> listCustomers(){
 		return new ResponseEntity<List<Customer>>(services.findAll(), HttpStatus.OK);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Customer> create(@RequestBody RequestDTO requestDTO){
+		return services.create(requestDTO);
 	}
 
 }
