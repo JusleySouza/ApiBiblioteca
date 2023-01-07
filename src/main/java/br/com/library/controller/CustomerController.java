208c03de3be0b1092a76f18f2f5fe07277875a70
Jusley Souza <jusleysouza31@gmail.com>
2023-01-07 17:04:11 -0300

    Implementation of the search route by cpf

--- src/main/java/br/com/library/controller/CustomerController.java
+++ src/main/java/br/com/library/controller/CustomerController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,6 +31,11 @@ public class CustomerController {
 	@PostMapping
 	public ResponseEntity<Customer> create(@RequestBody RequestDTO requestDTO){
 		return services.create(requestDTO);
+	}
+	
+	@GetMapping("/{customerCpf}")
+	public ResponseEntity<Customer> findByCpf(@PathVariable("customerCpf") String customerCpf){
+		return new ResponseEntity<Customer>(services.findByCpf(customerCpf), HttpStatus.OK);
 	}
 
 }
