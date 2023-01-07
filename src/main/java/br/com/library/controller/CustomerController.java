a805fc2c05475b02625d6fc3ccc373d3bbb44fef
Jusley Souza <jusleysouza31@gmail.com>
2023-01-07 17:34:55 -0300

    Implementation of the delete route

--- src/main/java/br/com/library/controller/CustomerController.java
+++ src/main/java/br/com/library/controller/CustomerController.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -45,4 +46,11 @@ public class CustomerController {
 			@PathVariable("customerId") UUID customerId){
 		return services.update(requestDTO, customerId);
 	}
+	
+	@DeleteMapping("/{customerId}")
+	public ResponseEntity<Customer> delete(@PathVariable("customerId") UUID customerId){
+		services.delete(customerId);
+		return new ResponseEntity<Customer>(HttpStatus.NO_CONTENT);
+	}
+	
 }
