85d42dc8b017ecbe40f0b9cd3303ec4290707f79
Jusley Souza <jusleysouza31@gmail.com>
2023-01-07 20:20:09 -0300

    Creation and implementation of the Address mapper

---
+++ src/main/java/br/com/library/mapper/AddressMapper.java
@@ -0,0 +1,23 @@
+package br.com.library.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+import br.com.library.model.Address;
+import br.com.library.model.dto.ViaCepDTO;
+
+@Mapper(componentModel = "spring")
+public interface AddressMapper {
+	
+	@Mappings({
+		@Mapping(target= "street", source  = "logradouro"),
+		@Mapping(target= "neighborhood", source = "bairro"),
+		@Mapping(target= "city", source = "localidade"),
+		@Mapping(target= "state", source = "uf"),
+		@Mapping(target= "id", ignore= true),
+		@Mapping(target= "number", ignore= true)
+	})
+	Address toModel(ViaCepDTO viaCepDTO);
+
+}
