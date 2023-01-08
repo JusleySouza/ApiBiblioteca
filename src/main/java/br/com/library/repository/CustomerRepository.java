b6f8eb3276bf174861c51c83fd855cbb8fab7d19
Jusley Souza <jusleysouza31@gmail.com>
2023-01-08 20:51:06 -0300

    Changing the return object and adding pageable

--- src/main/java/br/com/library/repository/CustomerRepository.java
+++ src/main/java/br/com/library/repository/CustomerRepository.java
@@ -1,8 +1,9 @@
 package br.com.library.repository;
 
-import java.util.List;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,6 +12,6 @@ import br.com.library.model.Customer;
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, UUID> {
 	Customer findByCpf(String cpf);
-	List<Customer> findAllByActiveTrue();
-	List<Customer> findAllByAddressCepAndActiveTrue(String cep);
+	Page<Customer> findAllByActiveTrue(Pageable pageable);
+	Page<Customer> findAllByAddressCepAndActiveTrue(String cep, Pageable pageable);
 }
