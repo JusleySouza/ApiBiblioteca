768b61eca27a4de7a9e65567616455e34057116b
Jusley Souza <jusleysouza31@gmail.com>
2023-01-08 17:31:11 -0300

    Adding validation messages

--- src/main/java/br/com/library/model/dto/RequestDTO.java
+++ src/main/java/br/com/library/model/dto/RequestDTO.java
@@ -1,5 +1,9 @@
 package br.com.library.model.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,10 +15,18 @@ import lombok.ToString;
 @NoArgsConstructor
 public class RequestDTO {
 
+	@NotEmpty(message = "{name.not.empty}")
 	private String name;
+	@NotEmpty(message = "{cpf.not.empty}")
 	private String cpf;
+	@NotEmpty(message = "{phone.not.empty}")
 	private String phone;
+	@Email(message = "{email.not.valid}")
+	@NotEmpty(message = "{email.not.empty}")
 	private String email;
+	@NotEmpty(message = "{cep.not.empty}")
 	private String cep;
+	@NotNull(message = "{number.not.null}")
+	@Min(value=1, message = "{number.not.less.than}")
 	private int number;
 }
