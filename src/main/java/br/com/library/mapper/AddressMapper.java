d443b80ee70a75aa402e28b17b2b677ed2bac0a1
Jusley Souza <jusleysouza31@gmail.com>
2023-01-10 14:55:46 -0300

    Modification in addressMapper class

--- src/main/java/br/com/library/mapper/AddressMapper.java
+++ src/main/java/br/com/library/mapper/AddressMapper.java
@@ -1,24 +1,33 @@
 package br.com.library.mapper;
 
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
-
 import br.com.library.model.Address;
+import br.com.library.model.Customer;
+import br.com.library.model.dto.RequestDTO;
+import br.com.library.model.dto.ResponseAddressDTO;
 import br.com.library.model.dto.ViaCepDTO;
 
-@Mapper(componentModel = "spring")
-public interface AddressMapper {
+public final class AddressMapper {
+
+	public static ResponseAddressDTO modelToResponseAddressDTO(Customer customer) {
+		return ResponseAddressDTO.builder()
+				.cep(customer.getAddress().getCep())
+				.number(customer.getAddress().getNumber())
+				.street(customer.getAddress().getStreet())
+				.neighborhood(customer.getAddress().getNeighborhood())
+				.city(customer.getAddress().getCity())
+				.state(customer.getAddress().getState())
+				.build();
+	}
 	
-	@Mappings({
-		@Mapping(target= "street", source  = "logradouro"),
-		@Mapping(target= "neighborhood", source = "bairro"),
-		@Mapping(target= "city", source = "localidade"),
-		@Mapping(target= "state", source = "uf"),
-		@Mapping(target= "id", ignore= true),
-		@Mapping(target= "cep", ignore= true),
-		@Mapping(target= "number", ignore= true)
-	})
-	Address toModel(ViaCepDTO viaCepDTO);
+	public static Address viaCepToModel(ViaCepDTO viaCepDTO, RequestDTO requestDTO) {
+		return Address.builder()
+				.street(viaCepDTO.getLogradouro())
+				.cep(requestDTO.getCep())
+				.number(requestDTO.getNumber())
+				.neighborhood(viaCepDTO.getBairro())
+				.city(viaCepDTO.getLocalidade())
+				.state(viaCepDTO.getUf())
+				.build();
+	}
 
 }
