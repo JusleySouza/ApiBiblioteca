8787c20d28012d3684d799f9bed15af2596b50e3
Jusley Souza <jusleysouza31@gmail.com>
2023-01-11 00:17:24 -0300

    Adding employee logger

--- src/main/java/br/com/library/config/LoggerConfig.java
+++ src/main/java/br/com/library/config/LoggerConfig.java
@@ -5,12 +5,14 @@ import org.slf4j.LoggerFactory;
 
 import br.com.library.services.implement.AddressServiceImplement;
 import br.com.library.services.implement.CustomerServiceImplement;
+import br.com.library.services.implement.EmployeeServiceImplement;
 import lombok.Generated;
 
 @Generated
 public class LoggerConfig {
 
 	public static final Logger LOGGER_CUSTOMER = LoggerFactory.getLogger(CustomerServiceImplement.class);
+	public static final Logger LOGGER_EMPLOYEE = LoggerFactory.getLogger(EmployeeServiceImplement.class);
 	public static final Logger LOGGER_ADDRESS = LoggerFactory.getLogger(AddressServiceImplement.class);
 	
 }
