2fd0660eb13bf0c1a0cfaa4c67793bb439e42b63
Jusley Souza <jusleysouza31@gmail.com>
2023-01-11 00:24:31 -0300

    Creation and implementation of the RequestEmployeeDTO model

---
+++ src/main/java/br/com/library/model/dto/RequestEmployeeDTO.java
@@ -0,0 +1,39 @@
+package br.com.library.model.dto;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RequestEmployeeDTO {
+
+	@NotEmpty(message = "{name.not.empty}")
+	private String name;
+	@NotEmpty(message = "{cpf.not.empty}")
+	private String cpf;
+	@NotEmpty(message = "{phone.not.empty}")
+	private String phone;
+	@Email(message = "{email.not.valid}")
+	@NotEmpty(message = "{email.not.empty}")
+	private String email;
+	@NotEmpty(message = "{position.not.empty}")
+	private String position;
+	@NotEmpty(message = "{cep.not.empty}")
+	private String cep;
+	@NotNull(message = "{number.not.null}")
+	@Min(value=1, message = "{number.not.less.than}")
+	private int number;
+}
