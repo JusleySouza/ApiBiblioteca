0865827554757087f21ec67f2a3d5fc304d16bba
Jusley Souza <jusleysouza31@gmail.com>
2023-01-11 00:42:56 -0300

    Adding employee paging

--- src/main/java/br/com/library/services/PaginationService.java
+++ src/main/java/br/com/library/services/PaginationService.java
@@ -6,12 +6,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 import br.com.library.model.Customer;
+import br.com.library.model.Employee;
 import br.com.library.model.dto.ListCustomer;
-import br.com.library.model.dto.ResponseDTO;
+import br.com.library.model.dto.ListEmployee;
+import br.com.library.model.dto.ResponseCustomerDTO;
+import br.com.library.model.dto.ResponseEmployeeDTO;
 
 @Service
 public interface PaginationService {
 
-	public ListCustomer pagination(Page<Customer> pageListCustomer, List<ResponseDTO> customer);
+	public ListCustomer paginationCustomer(Page<Customer> pageListCustomer, List<ResponseCustomerDTO> customer);
+	public ListEmployee paginationEmployee(Page<Employee> pageListEmployee, List<ResponseEmployeeDTO> employee);
 	
 }
