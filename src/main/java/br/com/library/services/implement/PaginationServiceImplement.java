da860eb803e43668910a41b71e02cb1d9b9eefc3
Jusley Souza <jusleysouza31@gmail.com>
2023-01-11 00:52:19 -0300

    Implementation of the paginationEmployee service

--- src/main/java/br/com/library/services/implement/PaginationServiceImplement.java
+++ src/main/java/br/com/library/services/implement/PaginationServiceImplement.java
@@ -6,18 +6,22 @@ import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 
 import br.com.library.model.Customer;
+import br.com.library.model.Employee;
 import br.com.library.model.dto.ListCustomer;
+import br.com.library.model.dto.ListEmployee;
 import br.com.library.model.dto.Pagination;
-import br.com.library.model.dto.ResponseDTO;
+import br.com.library.model.dto.ResponseCustomerDTO;
+import br.com.library.model.dto.ResponseEmployeeDTO;
 import br.com.library.services.PaginationService;
 
 @Component
 public class PaginationServiceImplement implements PaginationService {
 	private ListCustomer listCustomer;
+	private ListEmployee listEmployee;
 	private Pagination pagination;
 
 	@Override
-	public ListCustomer pagination(Page<Customer> pageListCustomer, List<ResponseDTO> customer) {
+	public ListCustomer paginationCustomer(Page<Customer> pageListCustomer, List<ResponseCustomerDTO> customer) {
 		pagination = new Pagination();
 		listCustomer = new ListCustomer();
 
@@ -33,6 +37,26 @@ public class PaginationServiceImplement implements PaginationService {
         listCustomer.setPageable(pagination);
 		
 		return listCustomer;
+	}
+
+	@Override
+	public ListEmployee paginationEmployee(Page<Employee> pageListEmployee, List<ResponseEmployeeDTO> employee) {
+
+		pagination = new Pagination();
+		listEmployee = new ListEmployee();
+
+		listEmployee.setContent(employee);
+		
+		pagination.setMoreElements(!pageListEmployee.isLast());
+		pagination.setOffset(pageListEmployee.getPageable().getOffset());
+        pagination.setPageNumber(pageListEmployee.getPageable().getPageNumber());
+        pagination.setPageSize(pageListEmployee.getPageable().getPageSize());
+        pagination.setTotalElements(pageListEmployee.getTotalElements());
+        pagination.setTotalPages(pageListEmployee.getTotalPages());
+        
+        listEmployee.setPageable(pagination);
+		
+		return listEmployee;
 	}
 
 }
