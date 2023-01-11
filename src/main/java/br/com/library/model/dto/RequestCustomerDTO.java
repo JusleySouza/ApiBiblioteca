0efa280e35e9d70ebe5a1634ae63e202a6e83f36
Jusley Souza <jusleysouza31@gmail.com>
2023-01-11 18:45:48 -0300

    Adding schema example

--- src/main/java/br/com/library/model/dto/RequestCustomerDTO.java
+++ src/main/java/br/com/library/model/dto/RequestCustomerDTO.java
@@ -5,6 +5,7 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -21,17 +22,23 @@ import lombok.ToString;
 public class RequestCustomerDTO {
 
 	@NotEmpty(message = "{name.not.empty}")
+	@Schema(description = "Employee name", type = "string", example = "Tiago Farias")
 	private String name;
 	@NotEmpty(message = "{cpf.not.empty}")
+	@Schema(description = "Employee cpf", type = "string", example = "97531426809")
 	private String cpf;
 	@NotEmpty(message = "{phone.not.empty}")
+	@Schema(description = "Employee phone", type = "string", example = "11985677685")
 	private String phone;
 	@Email(message = "{email.not.valid}")
 	@NotEmpty(message = "{email.not.empty}")
+	@Schema(description = "Employee email", type = "string", example = "tiago@farias.com")
 	private String email;
 	@NotEmpty(message = "{cep.not.empty}")
+	@Schema(description = "Employee cep", type = "string", example = "04777060")
 	private String cep;
 	@NotNull(message = "{number.not.null}")
 	@Min(value=1, message = "{number.not.less.than}")
+	@Schema(description = "Employee number", type = "string", example = "50")
 	private int number;
 }
--- src/main/java/br/com/library/model/dto/RequestEmployeeDTO.java
+++ src/main/java/br/com/library/model/dto/RequestEmployeeDTO.java
@@ -5,6 +5,7 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -21,19 +22,26 @@ import lombok.ToString;
 public class RequestEmployeeDTO {
 
 	@NotEmpty(message = "{name.not.empty}")
+    @Schema(description = "Employee name", type = "string", example = "Pedro Lima")
 	private String name;
 	@NotEmpty(message = "{cpf.not.empty}")
+	@Schema(description = "Employee cpf", type = "string", example = "13579864299")
 	private String cpf;
 	@NotEmpty(message = "{phone.not.empty}")
+	@Schema(description = "Employee phone", type = "string", example = "12987654321")
 	private String phone;
 	@Email(message = "{email.not.valid}")
 	@NotEmpty(message = "{email.not.empty}")
+	@Schema(description = "Employee email", type = "string", example = "pedro@lima.com")
 	private String email;
 	@NotEmpty(message = "{position.not.empty}")
+	@Schema(description = "Employee position", type = "string", example = "Gerente")
 	private String position;
 	@NotEmpty(message = "{cep.not.empty}")
+	@Schema(description = "Employee cep", type = "string", example = "04777060")
 	private String cep;
 	@NotNull(message = "{number.not.null}")
 	@Min(value=1, message = "{number.not.less.than}")
+	@Schema(description = "Employee number", type = "string", example = "2")
 	private int number;
 }
