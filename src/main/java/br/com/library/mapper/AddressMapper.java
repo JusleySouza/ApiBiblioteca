366ab0047a7aca62db7ef4604e38e2909044b1e0
Jusley Souza <jusleysouza31@gmail.com>
2023-01-11 00:29:44 -0300

    Adding employee modelToResponseAddressDTO and viaCepToModel

--- src/main/java/br/com/library/mapper/AddressMapper.java
+++ src/main/java/br/com/library/mapper/AddressMapper.java
@@ -2,7 +2,9 @@ package br.com.library.mapper;
 
 import br.com.library.model.Address;
 import br.com.library.model.Customer;
-import br.com.library.model.dto.RequestDTO;
+import br.com.library.model.Employee;
+import br.com.library.model.dto.RequestCustomerDTO;
+import br.com.library.model.dto.RequestEmployeeDTO;
 import br.com.library.model.dto.ResponseAddressDTO;
 import br.com.library.model.dto.ViaCepDTO;
 
@@ -19,7 +21,29 @@ public final class AddressMapper {
 				.build();
 	}
 	
-	public static Address viaCepToModel(ViaCepDTO viaCepDTO, RequestDTO requestDTO) {
+	public static ResponseAddressDTO modelToResponseAddressDTO(Employee employee) {
+		return ResponseAddressDTO.builder()
+				.cep(employee.getAddress().getCep())
+				.number(employee.getAddress().getNumber())
+				.street(employee.getAddress().getStreet())
+				.neighborhood(employee.getAddress().getNeighborhood())
+				.city(employee.getAddress().getCity())
+				.state(employee.getAddress().getState())
+				.build();
+	}
+	
+	public static Address viaCepToModel(ViaCepDTO viaCepDTO, RequestCustomerDTO requestDTO) {
+		return Address.builder()
+				.street(viaCepDTO.getLogradouro())
+				.cep(requestDTO.getCep())
+				.number(requestDTO.getNumber())
+				.neighborhood(viaCepDTO.getBairro())
+				.city(viaCepDTO.getLocalidade())
+				.state(viaCepDTO.getUf())
+				.build();
+	}
+	
+	public static Address viaCepToModel(ViaCepDTO viaCepDTO, RequestEmployeeDTO requestDTO) {
 		return Address.builder()
 				.street(viaCepDTO.getLogradouro())
 				.cep(requestDTO.getCep())
