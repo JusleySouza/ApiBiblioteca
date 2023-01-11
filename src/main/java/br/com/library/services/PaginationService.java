e7e19cc4f52bba9dbe253d263b1d8164393e482d
Jusley Souza <jusleysouza31@gmail.com>
2023-01-11 18:48:13 -0300

    Creation of the method to createPagination

--- src/main/java/br/com/library/services/PaginationService.java
+++ src/main/java/br/com/library/services/PaginationService.java
@@ -3,6 +3,7 @@ package br.com.library.services;
 import java.util.List;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import br.com.library.model.Customer;
@@ -17,5 +18,5 @@ public interface PaginationService {
 
 	public ListCustomer paginationCustomer(Page<Customer> pageListCustomer, List<ResponseCustomerDTO> customer);
 	public ListEmployee paginationEmployee(Page<Employee> pageListEmployee, List<ResponseEmployeeDTO> employee);
-	
+	public Pageable createPagination(Integer pageSize, Integer page, String sortBy);
 }
