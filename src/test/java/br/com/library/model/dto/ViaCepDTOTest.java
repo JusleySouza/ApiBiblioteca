c73d6d04a590bdf3de267e73ae06d438949e26ae
Jusley Souza <jusleysouza31@gmail.com>
2023-01-19 00:01:27 -0300

    Creation and implementation of the ViaCepDTO test class

---
+++ src/test/java/br/com/library/model/dto/ViaCepDTOTest.java
@@ -0,0 +1,31 @@
+package br.com.library.model.dto;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import br.com.library.test.utils.ClassBuilder;
+
+class ViaCepDTOTest {
+	
+	private ViaCepDTO expectedViaCepDTO;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		expectedViaCepDTO = ClassBuilder.viaCepDTOBuilder();
+	}
+
+	@Test
+	void setter() {	
+		ViaCepDTO viaCepDTO = new ViaCepDTO();
+		viaCepDTO.setCep("04447-010");;
+		viaCepDTO.setLogradouro("Av. Nossa Senhora do Sabará");;
+		viaCepDTO.setBairro("Jardim Campo Grande");;
+		viaCepDTO.setLocalidade("São Paulo");;
+		viaCepDTO.setUf("SP");;
+		viaCepDTO.setErro(false);
+		assertEquals(expectedViaCepDTO.toString(), viaCepDTO.toString());
+	}
+
+}
