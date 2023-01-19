22362e80b003f928f1c429fd812a778b0326b105
Jusley Souza <jusleysouza31@gmail.com>
2023-01-19 00:00:15 -0300

    Creation and implementation of the Response Customer test class

---
+++ src/test/java/br/com/library/model/dto/ResponseCustomerDTOTest.java
@@ -0,0 +1,51 @@
+package br.com.library.model.dto;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.UUID;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import br.com.library.test.utils.ClassBuilder;
+
+class ResponseCustomerDTOTest {
+	
+	private ResponseCustomerDTO expectedResponseCustomerDTO;
+	private ResponseAddressDTO responseAddressDTO;
+	private UUID id;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		id = UUID.randomUUID();
+		expectedResponseCustomerDTO = ClassBuilder.responseCustomerDTOBuilder();
+		responseAddressDTO = ClassBuilder.addressDTOBuilder();
+		expectedResponseCustomerDTO.setId(id);
+	}
+
+	@Test
+	void builder() {
+		ResponseCustomerDTO responseCustomerDTO = ResponseCustomerDTO.builder()
+				.id(id)
+				.name("Caio Castro")
+				.cpf("12345678965")
+				.phone("1231231231")
+				.email("caio@castro.com")
+				.responseAddressDTO(responseAddressDTO)
+				.build();
+		assertEquals(expectedResponseCustomerDTO.toString(), responseCustomerDTO.toString());
+	}
+
+	@Test
+	void setter() {
+		ResponseCustomerDTO responseCustomerDTO = new ResponseCustomerDTO();
+		responseCustomerDTO.setName("Caio Castro");
+		responseCustomerDTO.setId(id);
+		responseCustomerDTO.setCpf("12345678965");
+		responseCustomerDTO.setPhone("1231231231");
+		responseCustomerDTO.setEmail("caio@castro.com");
+		responseCustomerDTO.setResponseAddressDTO(responseAddressDTO);
+		assertEquals(expectedResponseCustomerDTO.toString(), responseCustomerDTO.toString());
+	}
+	
+}
